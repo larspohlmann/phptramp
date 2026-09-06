09f7248d37357417c0b100dfc31936f47f4da0e0
Lars Pohlmann <lars.pohlmann@googlemail.com>
2026-09-06 09:19:51 +0200

    chore: bump VERSION on develop to 0.3.0-dev after v0.2.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -45,7 +45,7 @@ use PhpTramp\Resolve\ClassHierarchy;
 final class Application
 {
     public const NAME = 'phptramp';
-    public const VERSION = '0.2.0';
+    public const VERSION = '0.3.0-dev';
 
     /** @var resource */
     private $stdout;
